f027ab2c1864a973916ad8c7ff7f3223bf7d5ffa
Lewis Taylor <lewis@dangerfarms.com>
2017-07-13 13:09:37 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 echo "Creating DB dump; better safe than sorry";
-docker exec ranchersecure_db_1 mysqldump -u cattle -pcattle cattle > dump.json
+docker exec ranchersecure_db_1 mysqldump -u cattle -pcattle cattle > dump.sql
 
 echo "Upgrading services"
 docker-compose stop; 
